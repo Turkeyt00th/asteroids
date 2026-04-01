ba9eaf45bc64b8d4dcdfd03c6d852290b129f9bc
Turkeyt00th <rory.h.walker@gmail.com>
2026-04-01 12:50:38 +0100

    Added acceleration to player movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_ACCELERATION, PLAYER_FRICTION, SHOT_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.shoot_cooldown_timer = 0
+        self.velocity = pygame.Vector2(0, 0)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -21,10 +22,21 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
-        unit_vector = pygame.Vector2(0, 1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        direction = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.velocity += direction * PLAYER_ACCELERATION * dt
+        if self.velocity.length() > PLAYER_SPEED:
+            self.velocity.scale_to_length(PLAYER_SPEED)
+
+    def apply_friction(self, dt):
+        if self.velocity.length_squared() == 0:
+            return
+
+        friction = PLAYER_FRICTION * dt
+        speed = self.velocity.length()
+        if friction >= speed:
+            self.velocity = pygame.Vector2(0, 0)
+        else:
+            self.velocity.scale_to_length(speed - friction)
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
@@ -40,8 +52,13 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
-        if keys[pygame.K_s]:
+        elif keys[pygame.K_s]:
             self.move(-dt)
+        else:
+            self.apply_friction(dt)
+
+        self.position += self.velocity * dt
+
         if keys[pygame.K_SPACE] and self.shoot_cooldown_timer <= 0:
             self.shoot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
             self.shoot()
