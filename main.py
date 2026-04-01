bb25096b5ddd492bcd8ccdfab67dc06ec89d8ab2
Turkeyt00th <rory.h.walker@gmail.com>
2026-04-01 12:41:12 +0100

    Added scoring system

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCORE_PER_ASTEROID
 from logger import log_event, log_state
 from player import Player
 from asteroid import Asteroid
@@ -12,11 +12,13 @@ from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    font = pygame.font.Font(None, 36)
 
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
 
+    score = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -42,18 +44,22 @@ def main():
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 log_event("player_hit")
-                print("Game over!")
+                print(f"Game over! Score: {score}")
                 sys.exit()
 
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
+                    score += SCORE_PER_ASTEROID
                     asteroid.split()
                     shot.kill()
 
         for sprite in drawable:
             sprite.draw(screen)
+
+        score_surface = font.render(f"Score: {score}", True, "white")
+        screen.blit(score_surface, (10, 10))
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
